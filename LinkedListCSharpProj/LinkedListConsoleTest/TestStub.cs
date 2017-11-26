bb8e8352af4eea819217ad0fc9b2167d9ef3f61c
Caydn Lofton <caydnlofton@gmail.com>
2017-11-26 12:00:04 -0800

    Initial Repo Setup

---
+++ LinkedListCSharpProj/LinkedListConsoleTest/TestStub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListConsoleTest
+{
+    class TestStub
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
